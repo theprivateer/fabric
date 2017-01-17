40cdcdb9b10491a37e97dd0fb9b60cd745328f3a
Phil Stephens <emailtheprivateer@gmail.com>
2017-01-17 16:08:04 +1000

    include published articles in sitemap.xml

--- src/Http/Controllers/SitemapController.php
+++ src/Http/Controllers/SitemapController.php
@@ -18,6 +18,12 @@ class SitemapController extends Controller
             if($page->id != site('homepage_id')) Sitemap::addTag(url($page->url), null, 'daily', '0.8');
         }
 
+        // Articles
+        foreach(site()->publishedArticles as $article)
+        {
+            Sitemap::addTag(url($article->prefixed_url), null, 'daily', '0.8');
+        }
+
         return Sitemap::render();
     }
 }
